e97485007bc247698c06202855ad3b8ef063d7d0
Bassant Mohammed <102248144+BassantM@users.noreply.github.com>
2025-01-03 00:02:11 +0200

    Add files via upload

---
+++ News Admin/app/src/main/java/com/maad/newsadmin/Article.kt
@@ -0,0 +1,8 @@
+package com.maad.newsadmin
+
+data class Article(
+    val title: String = "",
+    val desc: String = "",
+    val pic: String = "",
+    var id: String = "",
+)
---
+++ News Admin/app/src/main/java/com/maad/newsadmin/DetailsActivity.kt
@@ -0,0 +1,24 @@
+package com.maad.newsadmin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.maad.newsadmin.databinding.ActivityDetailsBinding
+
+class DetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.articleImageBtn.setOnClickListener { }
+
+        binding.doneImage.setOnClickListener {}
+
+    }
+}
